9823572f8d1fe9c54886b84cdd36e813bcc7825a
Rajendra Saha <virusspecialistrajendra789@gmail.com>
2025-06-08 12:09:58 +0530

    Add files via upload

---
+++ Netflix_Clone.js
@@ -0,0 +1,3 @@
+console.log("Netflix clone homepage loaded.");
+// This is a simple JavaScript file that simulates the loading of a Netflix clone homepage.
+// It logs a message to the console when the page is loaded.
